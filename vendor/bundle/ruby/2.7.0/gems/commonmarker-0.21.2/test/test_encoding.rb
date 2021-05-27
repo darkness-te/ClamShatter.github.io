2a4c6625be853fe41c0cfbbbbe83236888e9f784
Tanoan <tanoanbustos@gmail.com>
2021-05-26 17:46:56 -0700

    updoodle

---
+++ vendor/bundle/ruby/2.7.0/gems/commonmarker-0.21.2/test/test_encoding.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class TestEncoding < Minitest::Test
+  # see http://git.io/vq4FR
+  def test_encoding
+    contents = fixtures_file('curly.md')
+    doc = CommonMarker.render_doc(contents, :SMART)
+    render = doc.to_html
+    assert_equal('<p>This curly quote “makes commonmarker throw an exception”.</p>', render.rstrip)
+  end
+
+  def test_string_content_is_utf8
+    doc = CommonMarker.render_doc('Hi *there*')
+    text = doc.first_child.last_child.first_child
+    assert_equal('there', text.string_content)
+    assert_equal('UTF-8', text.string_content.encoding.name)
+  end
+end
